8692add91280229b6c49a40308df39e8ba96b288
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2022-03-26 14:35:37 +0300

    Deleting unnecessary files and add simple code (#2)
    1. Delete unnecessary files and folders
    2. Create simple controller and model - `project`
    3. Connect project to SQL Server with Entity Framework

---
+++ CollabPlatformApp/Contexts/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using CollabPlatformApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollabPlatformApp.Contexts
+{
+    public class ProjectContext : DbContext
+    {
+        public DbSet<Project>? Projects { get; set; }
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
