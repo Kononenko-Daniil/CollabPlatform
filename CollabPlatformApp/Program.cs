70cb1b6103b680930cc80523e39053a4761c8fe1
DESKTOP-E9Q37RT\Daniil <neokv210@gmail.com>
2022-04-26 23:09:27 +0300

    Refactor Program.cs

--- CollabPlatformApp/Program.cs
+++ CollabPlatformApp/Program.cs
@@ -8,38 +8,47 @@ using CollabPlatformApp.Validators;
 using CollabPlatformApp.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var AllowOrigins = "_allowOrigins";
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+string loginPath = "/users/sign-in";
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerGen();
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddDbContext<ProjectContext>(options => options.UseSqlServer(connection));
-builder.Services.AddScoped<UserValidator>();
+
+//Services
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 builder.Services.AddScoped<ILinkService, LinkService>();
 builder.Services.AddScoped<IUserService, UserService>();
+
+//Databases
+builder.Services.AddDbContext<ProjectContext>(options => options.UseSqlServer(connection));
+builder.Services.Configure<CollabPlatformDatabaseSettings>(
+    builder.Configuration.GetSection("CollabPlatformDatabase"));
+
+//Repositories
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ILinkRepository, LinkRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.Configure<CollabPlatformDatabaseSettings>(
-    builder.Configuration.GetSection("CollabPlatformDatabase"));
 
-builder.Services.AddCors(p => p.AddPolicy(AllowOrigins, builder =>
-{
-    builder.WithOrigins("https://localhost:44413").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-}));
-string loginPath = "/users/sign-in";
+//Validators
+builder.Services.AddScoped<UserValidator>();
+
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = new Microsoft.AspNetCore.Http.PathString(loginPath);
     });
 
-var app = builder.Build();
+builder.Services.AddCors(p => p.AddPolicy(AllowOrigins, builder =>
+{
+    builder.WithOrigins("https://localhost:44413").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+}));
 
+var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
