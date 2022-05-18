acf42d6614b717a45acc0276c35eca72bec34d4d
DESKTOP-E9Q37RT\Daniil <neokv210@gmail.com>
2022-05-18 13:53:40 +0300

    1. Add checking cookies to all pages. 2. Navbar in evere page component.

--- CollabPlatformApp/ClientApp/src/featured/create-project-page/component.js
+++ CollabPlatformApp/ClientApp/src/featured/create-project-page/component.js
@@ -2,30 +2,34 @@ import React from 'react';
 import {Container} from 'reactstrap';
 import Form from 'react-bootstrap/Form';
 import Button  from 'react-bootstrap/Button';
+import {NavbarContainer} from '../static-components/navbar/container';
 
 const CreateProjectComponent = (props) => {
-    const {projectName, 
+    const{projectName, 
         handleProjectNameChange, 
         handleProjectSubmit, 
         errorProjectNameMessage} = props;
 
     return(
-        <Container>
-            <h1 className={"pageName"}>Create project</h1>
-            <form  onSubmit={handleProjectSubmit}>
-                <p className="errorFormText">{errorProjectNameMessage}</p>
-                <Form.Control
-                        style={{width: "30% "}} 
-                        placeholder='Project title'
-                        value={projectName}
-                        className="inFormElements"
-                        type="input"
-                        id="inputProjectName"
-                        onChange={handleProjectNameChange}
-                    />
-                <Button variant="success" type="submit">Create</Button> 
-            </form>
-        </Container>
+        <div>
+            <NavbarContainer />
+            <Container>
+                <h1 className={"pageName"}>Create project</h1>
+                <form  onSubmit={handleProjectSubmit}>
+                    <p className="errorFormText">{errorProjectNameMessage}</p>
+                    <Form.Control
+                            style={{width: "30% "}} 
+                            placeholder='Project title'
+                            value={projectName}
+                            className="inFormElements"
+                            type="input"
+                            id="inputProjectName"
+                            onChange={handleProjectNameChange}
+                        />
+                    <Button variant="success" type="submit">Create</Button> 
+                </form>
+            </Container>
+        </div>
     )
 } 
 
--- CollabPlatformApp/ClientApp/src/featured/my-projects-page/component.js
+++ CollabPlatformApp/ClientApp/src/featured/my-projects-page/component.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ModalComponent from './modals/deleteProjectModal';
+import {NavbarContainer} from '../static-components/navbar/container';
 import {Container} from 'reactstrap';
 import { Link } from "react-router-dom";
 
@@ -26,53 +27,56 @@ const MyProjectsPageComponentFirstVariant = (props) =>{
     }
 
     return(
-        <Container>
-            <h1 className={"pageName"}>My projects</h1>
-            {
-                projects.length !== 0 ? 
-                <div>
-                    { 
-                        projects.map((project, index) => 
-                            <div className={'projectElement row'}> 
-                                <div className='col-lg-11' style={{paddingLeft:"-10px"}}>
-                                    <Link 
-                                        to={`/projects/${project.id}`} 
-                                        style={{color: "black", textDecoration: "none"}}>
-                                            <h5 className="projectNameSmall">
-                                                {project.name}
-                                            </h5>
-                                    </Link>
-                                    <p>
-                                        Tasks: {project.tasks.length} &ensp; 
-                                        Links: {project.links.length} &ensp; 
-                                        Contributors: {project.contributors.length}
-                                    </p>
+        <div>
+            <NavbarContainer />
+            <Container>
+                <h1 className={"pageName"}>My projects</h1>
+                {
+                    projects.length !== 0 ? 
+                    <div>
+                        { 
+                            projects.map((project, index) => 
+                                <div className={'projectElement row'}> 
+                                    <div className='col-lg-11' style={{paddingLeft:"-10px"}}>
+                                        <Link 
+                                            to={`/projects/${project.id}`} 
+                                            style={{color: "black", textDecoration: "none"}}>
+                                                <h5 className="projectNameSmall">
+                                                    {project.name}
+                                                </h5>
+                                        </Link>
+                                        <p>
+                                            Tasks: {project.tasks.length} &ensp; 
+                                            Links: {project.links.length} &ensp; 
+                                            Contributors: {project.contributors.length}
+                                        </p>
+                                    </div>
+                                    <div className='col'>
+                                        <Button 
+                                            onClick={() => handleShowDeleteModal(project.name, project.id)} 
+                                            variant='outline-danger'
+                                            style={{marginTop:"25px"}}>
+                                            Delete
+                                        </Button>
+                                    </div>
                                 </div>
-                                <div className='col'>
-                                    <Button 
-                                        onClick={() => handleShowDeleteModal(project.name, project.id)} 
-                                        variant='outline-danger'
-                                        style={{marginTop:"25px"}}>
-                                        Delete
-                                    </Button>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div> 
-                :
-                <div>
-                    <h5 className='doNotHave'>You don`t have any projects...</h5>
-                </div>
-            }
+                            )
+                        }
+                    </div> 
+                    :
+                    <div>
+                        <h5 className='doNotHave'>You don`t have any projects...</h5>
+                    </div>
+                }
 
-            <ModalComponent 
-                showDeleteProjectModal={showDeleteProjectModal}
-                projectName={projectName}
-                handleAcceptDelete={handleAcceptDelete}
-                handleCloseDeleteModal={handleCloseDeleteModal}
-                />
-        </Container>
+                <ModalComponent 
+                    showDeleteProjectModal={showDeleteProjectModal}
+                    projectName={projectName}
+                    handleAcceptDelete={handleAcceptDelete}
+                    handleCloseDeleteModal={handleCloseDeleteModal}
+                    />
+            </Container>
+        </div>
     )
 }
 
@@ -97,45 +101,48 @@ const MyProjectsPageComponentSecondVariant = (props) =>{
     }
 
     return(
-        <Container>
-            <h1 className={"pageName"}>My projects</h1>
-            {
-                projects.length !== 0 ? 
-                <div className={"row"}>
-                    { 
-                        projects.map((project, index) => 
-                            <Card className={'projectElementCard'} key={index}>
-                                <Card.Body>
-                                    <Card.Title>
-                                        {project.name}
-                                    </Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted tasksLinksNumText">
-                                        Tasks: {project.tasks.length} &ensp; Links: {project.links.length} 
-                                        <br/> Contributors: {project.contributors.length}
-                                    </Card.Subtitle>
-                                    <Link to={`/projects/${project.id}`}>
-                                        <Button style={{marginRight: "5px"}} variant='success'>Open project</Button>
-                                    </Link>
-                                    
-                                    <Button onClick={() => handleShowDeleteModal(project.name, project.id)} variant='outline-danger'>Delete</Button>
-                                </Card.Body>
-                            </Card>
-                        )
-                    }
-                </div> 
-                :
-                <div>
-                    <h5 className='doNotHave'>You don`t have any projects...</h5>
-                </div>
-            }
+        <div>
+            <NavbarContainer />
+            <Container>
+                <h1 className={"pageName"}>My projects</h1>
+                {
+                    projects.length !== 0 ? 
+                    <div className={"row"}>
+                        { 
+                            projects.map((project, index) => 
+                                <Card className={'projectElementCard'} key={index}>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            {project.name}
+                                        </Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted tasksLinksNumText">
+                                            Tasks: {project.tasks.length} &ensp; Links: {project.links.length} 
+                                            <br/> Contributors: {project.contributors.length}
+                                        </Card.Subtitle>
+                                        <Link to={`/projects/${project.id}`}>
+                                            <Button style={{marginRight: "5px"}} variant='success'>Open project</Button>
+                                        </Link>
+                                        
+                                        <Button onClick={() => handleShowDeleteModal(project.name, project.id)} variant='outline-danger'>Delete</Button>
+                                    </Card.Body>
+                                </Card>
+                            )
+                        }
+                    </div> 
+                    :
+                    <div>
+                        <h5 className='doNotHave'>You don`t have any projects...</h5>
+                    </div>
+                }
 
-            <ModalComponent 
-                showDeleteProjectModal={showDeleteProjectModal}
-                projectName={projectName}
-                handleAcceptDelete={handleAcceptDelete}
-                handleCloseDeleteModal={handleCloseDeleteModal}
-                />
-        </Container>
+                <ModalComponent 
+                    showDeleteProjectModal={showDeleteProjectModal}
+                    projectName={projectName}
+                    handleAcceptDelete={handleAcceptDelete}
+                    handleCloseDeleteModal={handleCloseDeleteModal}
+                    />
+            </Container>
+        </div>
     )
 }
 
